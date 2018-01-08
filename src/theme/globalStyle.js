9984359eccd4c8cc9090d6ef287a433829c37e64
Michael Jennings <MichaelJennings@Michaels-MacBook-Pro.local>
2018-01-08 18:10:38 -0500

    background image and layout

---
+++ src/theme/globalStyle.js
@@ -0,0 +1,10 @@
+import { injectGlobal } from 'styled-components';
+
+injectGlobal`
+  @import url('https://fonts.googleapis.com/css?family=Bellefair');
+
+body {
+  font-family: 'Bellefair', serif;
+  font-size: 24px;
+}
+`
